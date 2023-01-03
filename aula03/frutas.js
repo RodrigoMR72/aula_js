a8b54871300088eb54062ba7fc3be360fb0f606b
kevin <kevindiegodasilvasousa@gmail.com>
2023-01-02 21:47:17 -0300

    Atuaalização Aula 03

--- aula03/frutas.js
+++ aula03/frutas.js
@@ -52,7 +52,86 @@ if(posicao == -1) {
     console.log("Nome não encontrado");
     }
 else {
-    console.log("Nome encontrado: ", nomes[posicao]);
+    console.log("Nome encontrado na posição:",posicao, "é:", nomes[posicao]);
 }
 
-console.log(posicao);
+function falar(parametroFala){
+    console.log(parametroFala);
+}
+falar("Olá Ronier!!!");
+
+function gritar(parametroFala){
+    console.log(parametroFala.toUpperCase());
+}
+gritar("Olá Ronier!!!");
+
+function somar(p1, p2){
+    return p1 + p2;
+}
+
+console.log(somar(1,2));
+
+function imprimir(funcaoImprimir, texto){
+    funcaoImprimir (texto);
+}
+
+imprimir(console.log, "Olá mundo!!!");
+
+const salada = ["Maçã", "Uva", "Banana", "Mamão", "Pera"];
+
+function capsLk(valor){
+    return valor.toUpperCase();
+}
+
+const saladaUpper = salada.map(capsLk);
+
+console.log(salada);
+console.log(saladaUpper);
+
+
+// Modelo de ARROW FUNCTION.
+const saladaLow = salada.map((fruta)=> {return fruta.toLowerCase()});
+
+console.log(salada);
+console.log(saladaLow);
+
+
+const saladaNada = salada.map(()=> {return 'Nada'});
+
+console.log(salada);
+console.log(saladaNada);
+
+// Quando tenho só uma função no Arrow Function, não precisa de chaves ou return.
+const saladaUp = salada.map((fruta)=> fruta.toUpperCase());
+
+console.log(salada);
+console.log(saladaUp);
+
+const saladaUp1 = salada.map((fruta, index)=> fruta.toUpperCase() + index);
+
+console.log(salada);
+console.log(saladaUp1);
+
+const nomes1 = ["Ronier", "Larissa", "Vandecir", "Davi", "Alessandro", "Davi"];
+
+
+function filtrarNomes (nome) {
+
+    if (nome === "Davi")
+        return nome;
+}
+
+console.log(nomes1.filter(filtrarNomes));
+
+
+console.log(nomes1.includes("Ronier"));
+console.log(nomes1.includes("ronier"));
+
+
+function filtrarNomes1 (nome) {
+
+    if (nome.toUpperCase() === "Davi".toUpperCase)
+        return nome;
+}
+
+console.log(nomes1.filter(filtrarNomes1));
